571bff84a2ba2126eb54a3699a3a6f8ff37962ce
Gianluigi Provinciali <gianluigi.provinciali@gmail.com>
2025-10-28 12:11:23 +0100

    chore: clean unit test code

--- test/unit_test.go
+++ test/unit_test.go
@@ -2,7 +2,6 @@ package goplatform_test
 
 import (
 	"context"
-	"log"
 	"os"
 	"testing"
 
@@ -10,172 +9,170 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var (
-	URI            string
-	API_KEY        string
-	PROJECT_ID     string
-	NODE_ID        string
-	DEVICE_ID      string
-	DEVICE_TYPE_ID string
-	RULE_ID        string
-)
-
-func TestMain(m *testing.M) {
+func TestUnit(t *testing.T) {
 	if err := godotenv.Load("../.env"); err != nil {
-		log.Fatal(err)
-	}
-
-	URI = os.Getenv("URI")
-	API_KEY = os.Getenv("API_KEY")
-	PROJECT_ID = os.Getenv("PROJECT_ID")
-	NODE_ID = os.Getenv("NODE_ID")
-	DEVICE_ID = os.Getenv("DEVICE_ID")
-	DEVICE_TYPE_ID = os.Getenv("DEVICE_TYPE_ID")
-	RULE_ID = os.Getenv("RULE_ID")
-
-	exitVal := m.Run()
-	os.Exit(exitVal)
-}
-
-func TestGetProjects(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	if _, err := platform.GetProjects(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestGetProject(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	if _, err := platform.GetProject(PROJECT_ID); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestGetNodes(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
 		t.Fatal(err)
 	}
 
-	nodes, err := project.GetNodes()
-	if err != nil {
-		t.Fatal(err)
-	}
+	URI := os.Getenv("URI")
+	API_KEY := os.Getenv("API_KEY")
+	PROJECT_ID := os.Getenv("PROJECT_ID")
+	NODE_ID := os.Getenv("NODE_ID")
+	DEVICE_ID := os.Getenv("DEVICE_ID")
+	DEVICE_TYPE_ID := os.Getenv("DEVICE_TYPE_ID")
+	RULE_ID := os.Getenv("RULE_ID")
 
-	if len(nodes) == 0 {
-		t.Fail()
-	}
-}
+	t.Run("GetProjects", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-func TestGetNode(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if _, err := platform.GetProjects(); err != nil {
+			t.Fatal(err)
+		}
+	})
 
-	if _, err := project.GetNode(NODE_ID); err != nil {
-		t.Fatal(err)
-	}
-}
+	t.Run("GetProject", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-func TestGetDevices(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if _, err := platform.GetProject(PROJECT_ID); err != nil {
+			t.Fatal(err)
+		}
+	})
 
-	devices, err := project.GetDevices()
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("GetNodes", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-	if len(devices) == 0 {
-		t.Fail()
-	}
-}
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func TestGetDevice(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+		nodes, err := project.GetNodes()
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if _, err := project.GetDevice(DEVICE_ID); err != nil {
-		t.Fatal(err)
-	}
-}
+		if len(nodes) == 0 {
+			t.Fail()
+		}
+	})
 
-func TestGetDeviceTypes(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("GetNode", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-	devices, err := project.GetDeviceTypes()
-	if err != nil {
-		t.Fatal(err)
-	}
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(devices) == 0 {
-		t.Fail()
-	}
-}
+		if _, err := project.GetNode(NODE_ID); err != nil {
+			t.Fatal(err)
+		}
+	})
 
-func TestGetDeviceType(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("GetDevices", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-	if _, err := project.GetDeviceType(DEVICE_TYPE_ID); err != nil {
-		t.Fatal(err)
-	}
-}
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func TestCreateEvent(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+		devices, err := project.GetDevices()
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	event := goplatform.Event{
-		Description: "Evento di prova",
-		Type:        "Notification",
-		Source:      "test/event",
-	}
+		if len(devices) == 0 {
+			t.Fail()
+		}
+	})
 
-	if err := project.CreateEvent(event); err != nil {
-		t.Fatal(err)
-	}
-}
+	t.Run("GetDevice", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-func TestGetRules(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if _, err := project.GetRules(); err != nil {
-		t.Fatal(err)
-	}
-}
+		if _, err := project.GetDevice(DEVICE_ID); err != nil {
+			t.Fatal(err)
+		}
+	})
 
-func TestGetRule(t *testing.T) {
-	platform := goplatform.New(context.TODO(), URI, API_KEY)
-	project, err := platform.GetProject(PROJECT_ID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("GetDeviceTypes", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
 
-	if _, err := project.GetRule(RULE_ID); err != nil {
-		t.Fatal(err)
-	}
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		devices, err := project.GetDeviceTypes()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(devices) == 0 {
+			t.Fail()
+		}
+	})
+
+	t.Run("GetDeviceType", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
+
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := project.GetDeviceType(DEVICE_TYPE_ID); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("CreateEvent", func(t *testing.T) {
+		event := goplatform.Event{
+			Description: "Evento di prova",
+			Type:        "Notification",
+			Source:      "test/event",
+		}
+
+		platform := goplatform.New(context.Background(), URI, API_KEY)
+
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := project.CreateEvent(event); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("GetRules", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
+
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := project.GetRules(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("GetRule", func(t *testing.T) {
+		platform := goplatform.New(context.Background(), URI, API_KEY)
+
+		project, err := platform.GetProject(PROJECT_ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := project.GetRule(RULE_ID); err != nil {
+			t.Fatal(err)
+		}
+	})
 }
